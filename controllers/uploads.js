6aed3e5f61639095d517d6402d23f74dff7ac397
Luz Aramburo <shair.nash@gmail.com>
2022-04-06 15:34:39 -0600

    helper subir archivo

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,9 +1,7 @@
-const path = require("path");
-const { v4: uuidv4 } = require("uuid");
-
 const { response } = require("express");
+const { subirArchivo } = require("../helpers/subir-archivo");
 
-const cargarArchivo = (req, res = response) => {
+const cargarArchivo = async (req, res = response) => {
   console.log(req.files);
 
   if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
@@ -11,30 +9,9 @@ const cargarArchivo = (req, res = response) => {
     return;
   }
 
-  const { archivo } = req.files;
-  const nombreCortado = archivo.name.split(".");
-  const extension = nombreCortado[nombreCortado.length - 1];
+  const nombre = await subirArchivo(req.files);
 
-  // Calidar extensiones
-  const extensionesValidas = ["png", "jpg", "jpeg", "gif", "webp"];
-  if (!extensionesValidas.includes(extension)) {
-    return res.status(400).json({
-      msg: `La extensión ${extension} no está permitida, ${extensionesValidas}`,
-    });
-  }
-
-  const nombreTemp = uuidv4() + "." + extension;
-
-  const uploadPath = path.join(__dirname, "../uploads/", nombreTemp);
-
-  archivo.mv(uploadPath, (err) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).json({ err });
-    }
-
-    return res.json({ msg: "File uploaded to " + uploadPath });
-  });
+  return res.json({ nombre });
 };
 
 module.exports = {
